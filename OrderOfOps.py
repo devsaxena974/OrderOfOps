23f17ae74949877dcb0e029b00bf522fcf5e770c
Devanshu Saxena <devthegreat974@gmail.com>
2022-01-01 11:57:20 -0500

    Finalized the program. Modified use case

--- OrderOfOps.py
+++ OrderOfOps.py
@@ -1,51 +1,120 @@
 from Stack import Stack
 
 
-infix_stack = Stack()
-postfix_stack = Stack()
+class OrderOfOps:
 
-# first, we need to split up the string into individual characters and convert them to
-# to integers. Also wipe out any whitespace
+    def __init__(self, expression):
+        self.__infix_stack = Stack()
+        self.__postfix_stack = Stack()
+        self.__expression = expression
 
-# removing whitespace from expression
-
-
-def convert_to_postfix(expression):
-    output_string = ""
-    for i in expression:
-        if i == "(":
-            infix_stack.push(i)
-        elif i == "-" or i == "+":
-            infix_stack.push(i)
-        elif i == "*" or i == "/":
-            # if the last item in the stack is of greater or equal precedence, pop it
-            last_item = infix_stack.peek()
-            while last_item == "*" or last_item == "/":
-                output_string += infix_stack.pop()
-                last_item = infix_stack.peek()
-            infix_stack.push(i)
-        elif i == ")":
-            # while we don't come across an opening delimiter, keep popping operators
-            last_item = infix_stack.peek()
-            while last_item != "(":
-                output_string += infix_stack.pop()
-                last_item = infix_stack.peek()
-            infix_stack.pop()
-        else:
-            if str(i).isnumeric():
-                output_string += i
-            elif i == " ":
-                output_string += i
+    def __convert_to_postfix(self, expression):
+        output_string = ""
+        for i in expression:
+            if i == "(":
+                self.__infix_stack.push(i)
+            elif i == "-" or i == "+":
+                # while the last item in the stack is of greater or equal precedence, pop it
+                last_item = self.__infix_stack.peek()
+                while last_item == "*" or last_item == "/" or last_item == "+" or last_item == "-":
+                    output_string += self.__infix_stack.pop()
+                    last_item = self.__infix_stack.peek()
+                self.__infix_stack.push(i)
+            elif i == "*" or i == "/":
+                # while the last item in the stack is of greater or equal precedence, pop it
+                last_item = self.__infix_stack.peek()
+                while last_item == "*" or last_item == "/":
+                    output_string += self.__infix_stack.pop()
+                    last_item = self.__infix_stack.peek()
+                self.__infix_stack.push(i)
+            elif i == ")":
+                # while we don't come across an opening delimiter, keep popping operators
+                last_item = self.__infix_stack.peek()
+                while last_item != "(":
+                    output_string += self.__infix_stack.pop()
+                    last_item = self.__infix_stack.peek()
+                self.__infix_stack.pop()
             else:
-                raise ValueError
+                if str(i).isnumeric():
+                    output_string += i
+                elif i == " ":
+                    output_string += i
+                else:
+                    raise ValueError
 
-    for i in range(len(infix_stack)):
-        output_string += infix_stack.pop()
+        # pop everything else left in the stack
+        for i in range(len(self.__infix_stack)):
+            output_string += self.__infix_stack.pop()
 
-    print(infix_stack)
-    return output_string
+        return output_string
+
+    # Converting to an array makes it easier to keep multi-digit numbers apart from single digit ones
+    # and takes care of having to deal with whitespace when parsing through a string
+
+    def __convert_to_array(self, postfix_expression):
+        postfix_array = []
+        i = 0
+        while i in range(len(postfix_expression)):
+            if postfix_expression[i] == " ":
+                pass
+            elif postfix_expression[i].isnumeric() and postfix_expression[i+1] == " ":
+                postfix_array.append(int(postfix_expression[i]))
+            elif postfix_expression[i].isnumeric():
+                j = i
+                substring = ""
+                while postfix_expression[j] != " ":
+                    substring += postfix_expression[j]
+                    j += 1
+                    i += 1
+
+                postfix_array.append(int(substring))
+            elif postfix_expression[i] == "-":
+                postfix_array.append(postfix_expression[i])
+            elif postfix_expression[i] == "+":
+                postfix_array.append(postfix_expression[i])
+            elif postfix_expression[i] == "*":
+                postfix_array.append(postfix_expression[i])
+            elif postfix_expression[i] == "/":
+                postfix_array.append(postfix_expression[i])
+
+            i += 1
+
+        return postfix_array
+
+    def __evaluate_postfix(self, postfix_arr):
+        for i in postfix_arr:
+            if i == "-":
+                b = self.__postfix_stack.pop()
+                a = self.__postfix_stack.pop()
+                self.__postfix_stack.push(a - b)
+            elif i == "+":
+                b = self.__postfix_stack.pop()
+                a = self.__postfix_stack.pop()
+                self.__postfix_stack.push(a + b)
+            elif i == "*":
+                b = self.__postfix_stack.pop()
+                a = self.__postfix_stack.pop()
+                self.__postfix_stack.push(a * b)
+            elif i == "/":
+                b = self.__postfix_stack.pop()
+                a = self.__postfix_stack.pop()
+                self.__postfix_stack.push(a / b)
+
+            else:
+                self.__postfix_stack.push(i)
+
+        # The last item left in the stack is the calculated expression
+        return self.__postfix_stack.pop()
+
+    def calculate(self):
+        pfix_string = self.__convert_to_postfix(self.__expression)
+        pfix_arr = self.__convert_to_array(pfix_string)
+        result = self.__evaluate_postfix(pfix_arr)
+        return result
 
 
 if __name__ == "__main__":
-    ex = "( 4 * 12 ) + 1 "
-    print(convert_to_postfix(ex))
+    ex = "( 4 + 12 * 2 ) - 3 / 9 * ( 5 * 3 - 1 )"
+    obj = OrderOfOps(ex)
+    res = obj.calculate()
+    print(res)
